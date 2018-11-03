a7be60bba72ebf02b864ac79bede6c9b63f40641
Veertz <44732849+Veertz@users.noreply.github.com>
2018-11-03 23:25:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,14 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+	if (message.content == '!texturepack') {
+		message.channel.sendMessage('ZodiacHCF: http://www.mediafire.com/file/u73mo15efqik6fa/ZodiacHCF.zip/file');
+		message.channel.sendMessage('ZodiacHCF 6x6: http://www.mediafire.com/file/pt2rmukncjkj9hd/Zodiac_6x6.zip/file');
+	}
+	if (message.content == '!txp') {
+		message.channel.sendMessage('ZodiacHCF: http://www.mediafire.com/file/u73mo15efqik6fa/ZodiacHCF.zip/file');
+		message.channel.sendMessage('ZodiacHCF 6x6: http://www.mediafire.com/file/pt2rmukncjkj9hd/Zodiac_6x6.zip/file');
+	}
 });
 
 // THIS  MUST  BE  THIS  WAY
