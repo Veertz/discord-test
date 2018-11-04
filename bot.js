2ee9477cba0bb3c166ae6509d24e3d9fd2f646a2
Veertz <44732849+Veertz@users.noreply.github.com>
2018-11-04 20:35:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
+	await client.change_presence(game=discord.Game(name='Test'))
     console.log('I am ready!');
 });
 
