b67d73ac283125cccc218f57a822ccda2dcd96a0
Veertz <44732849+Veertz@users.noreply.github.com>
2018-11-04 21:05:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,6 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.content == '!ayuda') {
-		message.author.send("Holii");
-	}
-client.on('message', message => {
 	if (message.content == '!link') {
 		message.channel.sendMessage('https://discord.gg/4ke4Vm9');
 	}
