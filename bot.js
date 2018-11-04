6a1cea96521e7a37ae3bfb467a71e7a177693f7b
Veertz <44732849+Veertz@users.noreply.github.com>
2018-11-04 12:03:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,11 +14,6 @@ client.on('message', message => {
 		message.channel.sendMessage('ZodiacHCF: http://www.mediafire.com/file/u73mo15efqik6fa/ZodiacHCF.zip/file');
 		message.channel.sendMessage('ZodiacHCF 6x6: http://www.mediafire.com/file/pt2rmukncjkj9hd/Zodiac_6x6.zip/file');
 	}
-from .utils import checks
-
-@commands.command()
-# This under a command will check if the user is an admin or it has the manage server permission
-@checks.is_member() 
 });
 
 // THIS  MUST  BE  THIS  WAY
