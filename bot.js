e7a0018d80b545cfe0b79bdb43ebaae4fbdc835d
Veertz <44732849+Veertz@users.noreply.github.com>
2018-11-04 13:20:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,6 +10,15 @@ client.on('message', message => {
 		message.channel.sendMessage('https://discord.gg/4ke4Vm9');
 	}
 });
+public class status : ModuleBase<SocketCommandContext>
+{
+  private DiscordSocketClient _client;
+  public async Task StatusAsync()
+  {
+     var status = "eating doritos";
+     await _client.SetGameAsync(string status, string streamUrl = null, ActivityType type = Playing);
+  }
+}
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
