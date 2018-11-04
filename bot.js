05ebc230f29e57be570f45b0be6385077f386c00
Veertz <44732849+Veertz@users.noreply.github.com>
2018-11-04 20:54:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-	client.user.setGame("Game");
+	client.user.setGame("!ayuda");
     console.log('I am ready!');
 });
 
