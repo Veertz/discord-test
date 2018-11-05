d6233713b5584273b6d7d908f04774e17e2e7819
Veertz <44732849+Veertz@users.noreply.github.com>
2018-11-05 18:25:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,5 +12,12 @@ client.on('message', message => {
 	}
 });
 
+client.on("serverNewMember", (server, user) => {
+     client.sendMessage(user, "Bienvenido a: " + server.name);
+});
+
+client.on("message", (msg) => {
+     client.sendMessage(msg.author, "I can see you sent a message in: " + msg.channel.name + ", but I am just a simple bot and haven't been programmed to do anything special with messages yet.");
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
