a6c17003be603ea7f0c0c80fb093f3fb85b21122
Veertz <44732849+Veertz@users.noreply.github.com>
2018-11-05 18:32:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,14 +10,12 @@ client.on('message', message => {
 	if (message.content == '!link') {
 		message.channel.sendMessage('https://discord.gg/4ke4Vm9');
 	}
+
+client.on('message', message => {
+	if (message.content == '!ayuda') {
+		  client.sendMessage(msg.author, 'Usa !link para conseguir el codigo de invitacion permanente');
+	}
 });
 
-client.on("serverNewMember", (server, user) => {
-     client.sendMessage(user, "Bienvenido a: " + server.name);
-});
-
-client.on("message", (msg) => {
-     client.sendMessage(msg.author, "I can see you sent a message in: " + msg.channel.name + ", but I am just a simple bot and haven't been programmed to do anything special with messages yet.");
-});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
