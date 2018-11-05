b0f5c3516d2e2510bf5882e69539a7e67d1a2e24
Veertz <44732849+Veertz@users.noreply.github.com>
2018-11-05 18:36:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,11 +10,6 @@ client.on('message', message => {
 	if (message.content == '!link') {
 		message.channel.sendMessage('https://discord.gg/4ke4Vm9');
 	}
-
-client.on('message', message => {
-	if (message.content == '!ayuda') {
-		  client.sendMessage(msg.author, 'Usa !link para conseguir el codigo de invitacion permanente');
-	}
 });
 
 // THIS  MUST  BE  THIS  WAY
